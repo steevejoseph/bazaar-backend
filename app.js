ed071ae00d3b4a52a54c977b99a7ee0843e345e3
Steeve Joseph <steevejoseph@knights.ucf.edu>
2018-09-22 19:19:15 -0400

    add middleware folder + basic file + mlab dev link

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +13,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// database connection
+mongoose.connect('mongodb://team7:ABC123@ds263832.mlab.com:63832/largo-dev', {useNewUrlParser: true});
 
 app.use(logger('dev'));
 app.use(express.json());
---
+++ middleware/index.js
@@ -0,0 +1,7 @@
+var middleware = {};
+
+// isLoggedIn will probably go here
+
+// Also, authorization (different from Authentication) functions may/may not go here as well
+
+module.exports = middleware;
