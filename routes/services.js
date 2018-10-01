3b2e3ee6a2b98d978cf35f28da80385ca9a31e21
Steeve Joseph <steevejoseph@knights.ucf.edu>
2018-10-01 00:48:45 +0000

    half of search

--- routes/services.js
+++ routes/services.js
@@ -3,6 +3,10 @@ var express = require('express');
 var middleware = require('../middleware/index.js'),
     Service = require('../models/service.js'),
     mongoose = require('mongoose'),
+    request = require('request'),
+    urljoin = require('url-join'),
+    baseUrl = 'http://localhost:' + process.env.PORT,
+
     router  = express.Router();
     
     
@@ -19,11 +23,11 @@ router.get('/', function(req, res){
     });
 });
 
-router.get('/new', middleware.isLoggedIn, function(req, res){
+router.get('/new', function(req, res){
     res.render('services/new.ejs');
 });
 
-router.post('/new', middleware.isLoggedIn, function(req, res){
+router.post('/new', function(req, res){
     Service.create({
     name:req.body.name,
     owner:req.user,
@@ -39,6 +43,40 @@ router.post('/new', middleware.isLoggedIn, function(req, res){
             res.redirect('/services');
         }
     });
+});
+
+router.get('/search', function(req, res) {
+    res.render('services/search.ejs');
+});
+
+router.post('/search', function(req, res) {
+        var user;
+		
+		request({
+	    url: urljoin(baseUrl, '/api/users/signup'),
+	    method:'POST',
+	    json:{
+	        query:req.body.query,
+	    },
+	}, function(error, response, body){
+			if(error){
+				console.log(error);
+				console.log("error in POST search User side");
+			  // req.flash("failure", err.message);
+			  return res.redirect("/");
+			}
+	    console.log(body);
+	    // console.log
+	    if(response && response.statusCode == 200){
+				var results = body.results;
+				req.flash("success", "Successfully created account.");
+		  // changed rendering of login page to just go ahead and log-in
+		  // after signup.
+	        res.render('services/search_results.ejs', {results:results});
+	    }
+	    console.log('cant search, going home');
+	    res.render('dashboard.ejs');
+	});
 });
 
 module.exports = router;
