12c77d9cb05f0b9684128156ecb4bedb8686091b
Steeve Joseph <steevejoseph@knights.ucf.edu>
2018-10-05 06:06:12 +0000

    commit after testing mail

--- routes/index.js
+++ routes/index.js
@@ -91,32 +91,6 @@ router.post("/signup", function(req, res){
 	    
 	    res.redirect('/signup');
 	});
-
-	
-	
-	
-	// // console.log(req.body);
-	// User.register(new User({username:req.body.username}), req.body.password, function(err, user){
-	// 	if(err){
-	// 		console.log(err);
-	// 		req.flash("failure", err.message);
-	// 		return res.redirect("/signup");
-	// 	}
-		
-	// 	// Does multiple things:
-	// 	// 1. Logs user in.
-	// 	// 2. Takes care of all session info
-	// 	// 3. Stores correct info
-	// 	// 4. Runs serializeUser()
-	// 	// 5. Uses specified stratefy (local)
-	// 	// "local" can be changed to multiple different strats.
-	// 	passport.authenticate("local")(req, res, function(){
-	// 	req.flash("success", "Successfully created account.");
-	// 	// changed rendering of login page to just go ahead and log-in
-	// 	// after signup.
-	// 	res.redirect('/users/' + user._id);
-	// 	});
-	// });
 });
 
 
@@ -126,29 +100,7 @@ router.get("/login", function(req, res){
 
 // Perform authentication on login
 router.post("/login", function(req, res){
-	// passport.authenticate('local', function(err, user, info) {
- //   if (err) { 
- //   	console.log(err);
-	// 		req.flash("failure", err.message);
- //   	return res.redirect('/login');
-    	
- //   } if (!user) { 
- //   	console.log('Incorrect username or password');
- //   	req.flash('failure', info.message);
- //   	return res.redirect('/login'); 
- //   }
-    
- //   req.logIn(user, function(err) {
- //     if (err) { 
- //     	console.log(err);
- //     }
-    
- //     req.flash("success", "Successfully logged in.");
- //     res.redirect('/users/' + user._id);
- //   });
- // })(req, res);
- 
- 
+
  		var user;
 		
 		request({
