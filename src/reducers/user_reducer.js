4141766c1b15a0c63ec1d38a9de24e8cb286035f
Steeve Joseph <steevejoseph@knights.ucf.edu>
2018-11-14 17:51:16 -0500

    can now add services once logged in (fix auth header and data conversions

--- src/reducers/user_reducer.js
+++ src/reducers/user_reducer.js
@@ -7,21 +7,31 @@ export default function(state = {}, action) {
             return state;
         case LOGIN: 
             if (action.payload.status === 200) {
+                // console.log(typeof(action.payload.data.token));                
+                // console.log(action.payload.data);
                 localStorage.setItem('loggedInUser', JSON.stringify(action.payload.data.user))
-                return { 
+                localStorage.setItem('token', JSON.stringify(action.payload.data.token))
+                return {
+                    ...state,
                     loggedIn: true,
-                    user: action.payload.data.user
+                    user: action.payload.data.user,
+                    token: action.payload.data.token
                  };
             }
             return state;
         case GET_USER_FROM_LOCAL_STORAGE:
             return {
+                ...state,
                 loggedIn: true,
-                user: localStorage.getItem('loggedInUser')
+                user: localStorage.getItem('loggedInUser'),
+                token: localStorage.getItem('token')
+
             }
         case LOG_OUT_USER:
             return {
-                loggedIn: false
+                ...state,
+                loggedIn: false,
+
             }
         default:
             return state;
