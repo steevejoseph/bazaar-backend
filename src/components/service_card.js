7c4353be74f8afcf24dad35b65802d302cb89002
Nyasha <nyashafrank@knights.ucf.edu>
2018-11-18 12:52:22 -0500

    took off left button

--- src/components/service_card.js
+++ src/components/service_card.js
@@ -24,7 +24,6 @@ class ServiceCard extends Component {
                             <h6 className="card-subtitle mb-2 text-success">$15/hr</h6>
                             <p className="card-text">{this.props.service.description}</p>
                             <div className="btn-group" role="group" aria-label="...">
-                                <button type="button" className="btn btn-default">Left</button>
                                 <button type="button" className="btn btn-default">Edit</button>
                                 <button type="button" className="btn btn-danger">delete</button>
                             </div>
