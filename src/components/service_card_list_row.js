f0db41f276d010f52ef140cc0748b56c2a4209b1
abeSanchez <abraham5001@aol.com>
2018-11-18 16:40:37 -0500

    Couple of style changes

--- src/components/service_card_list_row.js
+++ src/components/service_card_list_row.js
@@ -6,11 +6,7 @@ export default class ServiceCardListRow extends Component {
     renderCardList() {
         return _.map(this.props.services, service => {
             return (
-<<<<<<< HEAD
-                <ServiceCard key={service._id} service={service}/>
-=======
-                    <ServiceCard key={service._id} service={service} ableToEdit={this.props.ableToEdit}/>
->>>>>>> 28b46e86a7ab1a34d4abda33c672fe286ba7b767
+                <ServiceCard key={service._id} service={service} ableToEdit={this.props.ableToEdit}/>
             )
         });
     }
