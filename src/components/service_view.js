5a946e3c7582cfb86bb50711d9d167770c7e7a84
Nyasha <nyashafrank@knights.ucf.edu>
2018-11-22 13:29:32 -0500

    skeleton to add review

--- src/components/service_view.js
+++ src/components/service_view.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { serviceView } from '../actions/index';
 import ServiceDescription from './service_description';
+import Rating from './service_rating';
+import CreateReview from './create_review';
 
 class ServiceView extends Component {
     componentDidMount() {
@@ -14,14 +16,22 @@ class ServiceView extends Component {
             return <div>Sorry, this service does not exist</div>;
 
         return ( 
-            <div>
+            <div className="container service-view">
                 <div><h1>{this.props.service.name}</h1></div>
+                <Rating />
                 <div><h4>{this.props.service.description}</h4></div>
+
                 <div><p><i className="fa fa-user-circle fa-3x"></i></p>
                     <h6>Seller name</h6>
                     <h6>Seller email</h6>
                 </div>
-                <ServiceDescription service={this.props.service} />
+                
+                <CreateReview serviceName={this.props.service.name} />
+
+                <div className="clear">
+                    <ServiceDescription service={this.props.service} />
+                </div>
+                
             </div>
         );
     }
