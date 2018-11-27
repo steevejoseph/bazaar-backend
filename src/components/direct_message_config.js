64f7c8c1e6918d85e5ee6c64555ea0de2284b496
Nyasha <nyashafrank@knights.ucf.edu>
2018-11-26 20:44:40 -0500

    always forgetting the untracked

---
+++ src/components/direct_message_config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/b90a149b-4af4-4243-9831-b133bff9a54e/token";
+const instanceLocator = "v1:us1:b90a149b-4af4-4243-9831-b133bff9a54e";
+
+export { tokenUrl, instanceLocator }
---
+++ src/components/direct_messages_room_list.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class RoomList extends Component {
+    render(){
+        return (
+            <div className="rooms-list">
+                <ul>
+                    <h4 className="font-weight-bold">Direct Messages</h4>
+
+                    {this.props.rooms.map(room => {
+                        const active = this.props.roomId === room.id ? 'active' : '';
+                        return (
+                            <li key={room.id} className={"room " + active}>
+                                <a 
+                                    onClick={() => this.props.subscribeToRoom(room.id)}
+                                    href="#"
+                                >
+                                    {room.name}
+                                </a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default RoomList;
